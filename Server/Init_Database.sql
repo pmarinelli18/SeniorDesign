2868ea03766323c59f4970a0fb2fa6cf2c6b9b88
Mark Lawrence <mpmjlaw@icloud.com>
2021-02-11 16:31:13 -0500

    Updated some of table schema for the boat state table

--- Server/Init_Database.sql
+++ Server/Init_Database.sql
@@ -2,14 +2,30 @@ CREATE DATABASE BattleShip;
 USE BattleShip;
 
 Create Table Accounts(
-	userName VARCHAR(50), 
+    userName VARCHAR(50), 
     password VARCHAR(50),
     PRIMARY KEY(userName)
 );
 
 Create Table BoatState(
-	UserID VARCHAR(50),
-	navigationPosition INTEGER DEFAULT 0, 
+    ipAddress VARCHAR(50),
+    cannonActive BOOL DEFAULT false, 
+    mountedMGActive BOOL DEFAULT false, 
+    torpedoActive BOOL DEFAULT false, 
+    radarActive BOOL DEFAULT false, 
+    shipHealthStatus DOUBLE DEFAULT 1.0,
+    PRIMARY KEY(ipAddress)
+);
+
+INSERT INTO Accounts(userName, password) 
+    Values
+    ('userName 1', 'password'),
+    ('userName 2', 'password2');
+    
+Create Table BoatState(
+    IpAddress VARCHAR(50),
+    UserName VARCHAR(50),
+    navigationPosition INTEGER DEFAULT 0, 
     RadarState VARCHAR(15) DEFAULT 'Enabled', 
     ShipHealth INTEGER DEFAULT 100, 
     NumberOfCannons INTEGER DEFAULT 3, 
@@ -17,12 +33,5 @@ Create Table BoatState(
     TorpedoDamage INTEGER DEFAULT 0,
     FinishedMiniGame BOOL DEFAULT false,
     GameActive BOOL DEFAULT true,
-    PRIMARY KEY(UserID)
+    PRIMARY KEY(ipAddress)
 );
-
-INSERT INTO Accounts(userName, password) 
-	Values
-	('userName 1', 'password'),
-    ('userName 2', 'password2');
-    
-
